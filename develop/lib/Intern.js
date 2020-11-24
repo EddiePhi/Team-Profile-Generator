3ca490cb4ff5724378b6d5fa03992cf74ab393cc
Eddie Phimansone <ephimansone@gmail.com>
2020-11-24 17:46:55 -0500

    created questions, setup inquirer logic, able to produce blank team.html page

---
+++ develop/lib/Intern.js
@@ -0,0 +1,13 @@
+// TODO: Write code to define and export the Intern class.  HINT: This class should inherit from Employee.
+const Employee = require("./Employee")
+
+class Intern extends Employee {
+    constructor(school){
+        this.school = school;
+        super(name, id, email);
+    };
+    getSchool(){};
+    getRole(){
+        return "Intern"
+    };
+};
